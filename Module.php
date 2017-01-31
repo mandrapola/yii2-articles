32b9dbec5a4bba3195a21e3624f359ca16c8bf84
Khusainov Marat <hmf@yandex.ru>
2017-01-31 12:01:13 +0300

    Controller path edit

--- Module.php
+++ Module.php
@@ -11,19 +11,5 @@ class Module extends \yii\base\Module
     /**
      * @inheritdoc
      */
-    public $controllerNamespace = 'article\controllers';
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-        if (!isset(\Yii::$app->get('i18n')->translations['article*'])) {
-            \Yii::$app->get('i18n')->translations['article*'] = [
-                'class' => PhpMessageSource::className(),
-                'basePath' => __DIR__.'/messages',
-            ];
-        }
-    }
+    public $controllerNamespace = 'mandrapola\article\controllers';
 }
