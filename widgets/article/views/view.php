a73c18d43a21650093965f9a54957c97ab4f88dd
Khusainov Marat <hmf@yandex.ru>
2017-05-31 15:26:56 +0300

    виджет Article вывод title

--- widgets/article/views/view.php
+++ widgets/article/views/view.php
@@ -6,7 +6,7 @@
  * Time: 15:49
  */
 
-$this->title .= ' ' . $model->title;
+
 $this->params['breadcrumbs'] = [['label' => $model->title]];
 
 $content = \yii\helpers\Html::tag('h1', $model->title) . Yii::$app->formatter->asRaw($model->body);
